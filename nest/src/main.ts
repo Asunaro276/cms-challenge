f14b64d711d6906e7ef381cd2e46077f13484003
asunaro276 <ryuhei276276@gmail.com>
2024-01-27 00:17:16 +0900

    [update] nestでcorsを許可

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -3,6 +3,7 @@ import { RootModule } from './module/.RootModule'
 
 async function bootstrap() {
   const app = await NestFactory.create(RootModule);
+  app.enableCors()
   await app.listen(3000);
 }
 bootstrap();
