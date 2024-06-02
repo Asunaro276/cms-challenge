9d82c07c50e6b04a4e0a61fcfddca42239711428
Asunaro276 <ryuhei276276@gmail.com>
2024-06-03 01:33:15 +0900

    [update] nestのportを8080二変更

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -4,6 +4,6 @@ import { RootModule } from './module/.RootModule'
 async function bootstrap() {
   const app = await NestFactory.create(RootModule);
   app.enableCors()
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
